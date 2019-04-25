bc29cd84fc038bef5353f110cc8ccf032ed15b2b
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-04-24 22:24:00 -0400

    Add Menu class interface.

--- Menu.hh
+++ Menu.hh
@@ -3,25 +3,27 @@
 #include <string>
 #include <functional>
 #include <vector>
+#include <iostream>
 
-template <class T>
 class Menu
 {
 private:
   struct Option {
     std::string entry;
     std::string description;
-    std::function<void (T&)> action;
+    std::function<void ()> action;
   };
 
   std::vector<Option> options;
   std::function<void (const std::string&, const std:: string&)> displayOption;
-  void defaultFormat(const std::string&, const std:: string&) const;
+  static void defaultOptionOutput(const std::string&, const std:: string&);
+  static void defaultInvalidInputResponse();
+  bool matchResponse(const std::string&) const;
 
 public:
-  Menu(std::initializer_list<Option>, std::function<void (const std::string&, const std:: string&)> displayFormat = defaultFormat);
-  void display();
-  void displayUntilValid();
+  Menu(std::initializer_list<Option>, std::function<void (const std::string&, const std:: string&)> displayFormat = [] (const std::string& a, const std::string& b) {defaultOptionOutput(a, b);});
+  void display(std::function<void ()> invalidInputEvent = [] (){defaultInvalidInputResponse();});
+  void displayUntilValid(std::function<void ()> invalidInputEvent = [] (){defaultInvalidInputResponse();});
 };
 
 #endif
